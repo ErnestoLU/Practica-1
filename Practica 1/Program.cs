dc010d4a48db182fa0a103fc3ed5e9e97d925c21
usuario <usuario@ERNI-PC>
2022-09-13 17:39:51 -0700

    Agregar archivos de proyecto.

---
+++ Practica 1/Program.cs
@@ -0,0 +1,23 @@
+﻿
+
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace Practica1
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+           
+
+            Console.WriteLine("Ernesto Leon, Tecate Baja California, 21 años.");
+            Console.ReadKey();
+        }
+    }
+}
